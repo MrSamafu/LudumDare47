f51e715eef2f39c1cc19d8a6cebc62f67c372ae3
Samson <51507673+MrSamafu@users.noreply.github.com>
2020-10-03 16:31:48 +0200

    Add doors

---
+++ Assets/RandomDoor.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomDoor : MonoBehaviour
+{
+    public GameObject[] doors;
+    
+
+    void Start()
+    {
+        foreach(GameObject door in doors)
+        {
+            int x = Random.Range(0, 2);
+            if (x == 0)
+            {
+                door.GetComponent<Animator>().SetBool("open", true);
+            }
+            else if (x == 1)
+            {
+                door.GetComponent<Animator>().SetBool("open", false);
+
+            }
+            Debug.Log(x);
+        }
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
