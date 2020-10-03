a5f63aa216c1a58c51de5196e45e4625053e55af
Samson <51507673+MrSamafu@users.noreply.github.com>
2020-10-03 05:03:02 +0200

    add item generator

---
+++ Assets/ItemGenerator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ItemGenerator : MonoBehaviour
+{
+    public GameObject pile;
+    public GameObject[] itemGenerator;
+
+    private void Start()
+    {
+        int x = Random.Range(0, 9);
+        Instantiate(pile, itemGenerator[x].transform);
+    }
+}
