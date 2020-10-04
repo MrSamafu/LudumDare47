c18a879c544b037a0b059cfda1b201cdee2f52e1
Samson <51507673+MrSamafu@users.noreply.github.com>
2020-10-04 22:30:27 +0200

    Add WinScene + GameOver

--- Assets/PlayerStat.cs
+++ Assets/PlayerStat.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class PlayerStat : MonoBehaviour
 {
@@ -11,9 +12,23 @@ public class PlayerStat : MonoBehaviour
     public int battery;
     public int key;
     public TMP_Text batteryText;
+    public Image lifeBar;
+    public TMP_Text keyText;
+    public GameObject gameOver;
 
     private void Update()
     {
-        batteryText.text = "Battery : " + battery;
+        keyText.text = "Key : " + key + "/2";
+        lifeBar.fillAmount = ((life * 100)/100)/100;
+        
+        if(life <= 0)
+        {
+            gameOver.SetActive(true);
+            GetComponent<PlayerMove>().enabled = false;
+            GetComponent<SpriteRenderer>().enabled = false;
+            GetComponent<Rigidbody>().isKinematic = true;
+
+        }
+
     }
 }
---
+++ Assets/TheEnd.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+
+public class TheEnd : MonoBehaviour
+{
+    public TMP_Text score;
+    void Start()
+    {
+        score.text = "Score : " + PlayerPrefs.GetString("Time");
+
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            SceneManager.LoadScene("Credit");
+        }
+    }
+}
