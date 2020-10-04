20332c6783a57f7b55eebc3cd840749f0312ec35
Samson <51507673+MrSamafu@users.noreply.github.com>
2020-10-04 17:30:32 +0200

    Add LightGate

---
+++ Assets/GateScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GateScript : MonoBehaviour
+{
+    public GameObject[] lightGate;
+    public Material matLightOn;
+    public Material matLightOff;
+
+    private PlayerStat playerStat;
+
+    private void Start()
+    {
+        playerStat = GameObject.Find("Player").GetComponent<PlayerStat>();
+        foreach (GameObject lightInit in lightGate)
+        {
+            lightInit.GetComponent<MeshRenderer>().material = matLightOff;
+        }
+
+    }
+    private void Update()
+    {
+        int battery = playerStat.battery;
+
+        if(battery > 0 && battery <= 5)
+        {
+            for (int i = 0; i < battery;i++)
+            {
+                lightGate[i].GetComponent<MeshRenderer>().material = matLightOn;
+            }
+        }
+
+
+    }
+}
