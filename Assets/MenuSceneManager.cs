1de20458f173083c1380806ea44cfa8e5c65b19f
Samson <51507673+MrSamafu@users.noreply.github.com>
2020-10-04 19:53:29 +0200

    Update Menu
    Permet de lancer le jeux

---
+++ Assets/MenuSceneManager.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSceneManager : MonoBehaviour
+{
+   public void PlayGame()
+    {
+        SceneManager.LoadScene("LevelDesign");
+    }
+}
