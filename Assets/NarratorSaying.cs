bd87f2344bb874e8826d90e1f2be5e52d8fea754
Samson <51507673+MrSamafu@users.noreply.github.com>
2020-10-05 14:17:10 +0200

    Add Narrator

---
+++ Assets/NarratorSaying.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class NarratorSaying : MonoBehaviour
+{
+    public TMP_Text narratorSay;
+    public Scrollbar content;
+    private int count;
+    public string[] sentence;
+
+    void Start()
+    {
+        count = 0;
+        StartCoroutine(speak());
+    }
+    IEnumerator speak()
+    {
+        yield return new WaitForSeconds(Random.Range(5f, 10f));
+        narratorSay.text = narratorSay.text + sentence[count] + "<br>";
+        count++;
+        if(count == sentence.Length)
+        {
+
+        }
+        else
+        {
+            StartCoroutine(speak());
+            content.value -= 0.025f;
+        }
+    }
+}
